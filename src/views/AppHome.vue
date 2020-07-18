e0666d56267f252891a1373b0347cbbcaf291205
강경태 <gyeongtae020319@gmail.com>
2020-07-18 11:57:06 +0900

    [UPDATE] Add design sketch

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -7,6 +7,61 @@
             </h1>
             <hackathon-timer eventname="apply-start" class="timer" />
         </div>
+
+        <div class="calander">
+            <table>
+                <tr>
+                    <th colspan="2">8월 7일</th>
+                </tr>
+                <tr>
+                    <th>시간</th>
+                    <th>일정</th>
+                </tr>
+                <tr>
+                    <td>13:00</td>
+                    <td>시작</td>
+                </tr>
+            </table>
+            <table>
+                <tr>
+                    <th colspan="2">8월 8일</th>
+                </tr>
+                <tr>
+                    <th>시간</th>
+                    <th>일정</th>
+                </tr>
+                <tr>
+                    <td>13:00</td>
+                    <td>시작</td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="menu">
+            <span class="title">선린 메뉴판</span>
+            <div class="contents">
+                <div class="left">
+                    <div class="item">
+                        <span class="name">코카콜라</span>
+                        <span class="price">1,000P</span>
+                    </div>
+                    <div class="item">
+                        <span class="name">몬스터</span>
+                        <span class="price">2,000P</span>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="item">
+                        <span class="name">펩시</span>
+                        <span class="price">10,000P</span>
+                    </div>
+                    <div class="item">
+                        <span class="name">100P</span>
+                        <span class="price">1,000P</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
